8e65f751da1aaeb4f0c5f040aebae1d78ebbf674
ritiksharmarj <bloggeritik@gmail.com>
2022-05-26 15:32:52 +0530

    second commit

---
+++ gameplay.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelector('.show-modal');
+
+btnShowModal.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+})
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
